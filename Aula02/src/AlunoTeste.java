6c6432920dfec0c9e61ca606bcff087de42fc4bf
raphatatto <raphaelatatto@gmail.com>
2024-03-10 22:17:37 -0300

    calculando média

--- Aula02/src/AlunoTeste.java
+++ Aula02/src/AlunoTeste.java
@@ -7,9 +7,17 @@ public class AlunoTeste {
         //Ler os dados: nome, nota gs, cp e challenge
         System.out.println("Digite o nome:");
         String nome = scanner.next() + scanner.nextLine();
+        System.out.println("Digite a nota da gs");
+        float gs = scanner.nextFloat();
+        System.out.println("Digite a nota da cp");
+        float cp = scanner.nextFloat();
+        System.out.println("Digite a nota da challenge");
+        float challenge = scanner.nextFloat();
+
 
         //Calcular e exibir a media do aluno
-
+        void mediaFinal() { System.out.println(nome + " sua média foi calculada..");
+        }
 
     }
 }
